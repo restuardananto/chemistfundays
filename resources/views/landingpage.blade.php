b75e7b7ce8ff19d7ac83399da8c85373ceb095eb
restuardananto <stuard.phone@gmail.com>
2021-08-08 17:29:42 +0800

    navbar landing page

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,12 +1,33 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Chemist Fun Days</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="{!! asset('assets/csscustom/style.css') !!}" rel="stylesheet">
+
+    <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Landing Page</h1>
-    <h3>disini dikasih tampilan tutorial cara login dan sebagainya</h3>
-    <button><a href="/loginpeserta">login peserta</a></button>
-    <button><a href="/loginpanitia">login panitia</a></button>
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container d-flex flex-row justify-content-start">
+        <a class="navbar-brand" href="/">
+          <img src="{!! asset('assets/img/vector-logo.png') !!}" height="50" width="120"></a>
+        </a>
+          <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link" href="/signuppesertapage">buat akun</a>
+              <a href="/loginpesertapage">
+                <button type="button" class="tombol btn">Masuk</button>
+              </a>
+            </div>
+          </div>
+      </div>
+    </nav>
   </body>
 </html>
