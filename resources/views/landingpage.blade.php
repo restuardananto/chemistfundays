98945ab5c359acf08372450f308f789f0ba236a5
restuardananto <stuard.phone@gmail.com>
2021-08-11 09:02:11 +0800

    landingpage210811

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ChemistFunDays</title>
+</head>
+<body>
+  <div class="navbar">
+    <a href="/">Home</a>
+    <a href="/loginpesertapage">Login</a>
+    <a href="/signuppesertapage">Signup</a>
+    <a href=""><img src="" alt=""></a>
+  </div>
+  <section class="sec1">
+    <div class="sec1bagkiri">
+      <img src="" alt="">
+      <div class="unmullogo">
+        <img src="" alt="">
+        <img src="" alt="">
+      </div>
+      <div class="tagnbutton">
+        <h3>CHEMIST FUN DAYS XIII</h3>
+        <p>kompetisi kimia yang akan membawamu baradu ketangkasan  mengasah otak dalam perlombaan kimia yang disediakan dengan cara yang menyenangkan</p>
+        <a href="/signuppesertapage">DAFTAR SEKARANG!</a>
+      </div>
+    </div>
+    <div class="sec1bagkanan">
+      <img src="" alt="">
+    </div>
+  </section>
+  <section class="sec2">
+    <div class="sec2bagkiri">
+      <img src="" alt="">
+    </div>
+    <div class="sec2bagkanan">
+      <img src="" alt="">
+    </div>
+  </section>
+</body>
+</html>
