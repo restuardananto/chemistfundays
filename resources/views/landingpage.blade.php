ccf3b172dc723424eec10a7b94e63d15b977d230
restuardananto <stuard.phone@gmail.com>
2021-08-11 15:32:40 +0800

    landing page 210811-15:32

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -4,39 +4,38 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{!! asset('assets/csscustom/landingpage.css') !!}">
   <title>ChemistFunDays</title>
 </head>
 <body>
-  <div class="navbar">
-    <a href="/">Home</a>
-    <a href="/loginpesertapage">Login</a>
-    <a href="/signuppesertapage">Signup</a>
-    <a href=""><img src="" alt=""></a>
+  <div class="konten">
+    <div class="navbar">
+      <a href="/">Home</a>
+      <a href="/loginpesertapage">Login</a>
+      <a href="/signuppesertapage">Signup</a>
+      <a href=""><img src="" alt=""></a>
+    </div>
+    <div class="sec1">
+      <div class="sec1bagkiri">
+        <img src="{!! asset('assets/img/sec1logo.svg') !!}" alt="">
+        <div class="tagnbutton">
+          <h3>CHEMIST FUN DAYS XIII</h3>
+          <p>kompetisi kimia yang akan membawamu baradu ketangkasan  mengasah otak dalam perlombaan kimia yang disediakan dengan cara yang menyenangkan</p>
+          <a href="/signuppesertapage" class="a-button">DAFTAR SEKARANG!</a>
+        </div>
+      </div>
+      <div class="sec1bagkanan">
+        <img src="{!! asset('assets/img/sec1illustrasi.svg') !!}" alt="">
+      </div>
+    </div>
+    <div class="sec2">
+      <div class="sec2bagkiri">
+        <img src="{!! asset('assets/img/sec2illustrasi.svg') !!}" alt="">
+      </div>
+      <div class="sec2bagkanan">
+        <img src="{!! asset('assets/img/sec2tutorial.svg') !!}" alt="">
+      </div>
+    </div>
   </div>
-  <section class="sec1">
-    <div class="sec1bagkiri">
-      <img src="" alt="">
-      <div class="unmullogo">
-        <img src="" alt="">
-        <img src="" alt="">
-      </div>
-      <div class="tagnbutton">
-        <h3>CHEMIST FUN DAYS XIII</h3>
-        <p>kompetisi kimia yang akan membawamu baradu ketangkasan  mengasah otak dalam perlombaan kimia yang disediakan dengan cara yang menyenangkan</p>
-        <a href="/signuppesertapage">DAFTAR SEKARANG!</a>
-      </div>
-    </div>
-    <div class="sec1bagkanan">
-      <img src="" alt="">
-    </div>
-  </section>
-  <section class="sec2">
-    <div class="sec2bagkiri">
-      <img src="" alt="">
-    </div>
-    <div class="sec2bagkanan">
-      <img src="" alt="">
-    </div>
-  </section>
 </body>
 </html>
